e8ba964dfa861bb63904384d58aff294851d546f
tubuianh <anhtu2122002@gmail.com>
2024-01-16 13:15:42 +0700

    Lab1Bai1

---
+++ Lab1_TenSV_Diem/src/Lab1Bai1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+public class Lab1Bai1 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in); 
+		System.out.print("Họ và tên: ");
+		String hoTen = scanner.nextLine();
+		System.out.print("Điểm TB: ");
+		double diemTB = scanner.nextDouble();
+		
+		System.out.printf("Họ tên: %s, Điểm: %f",hoTen,diemTB);
+	}
+
+}
